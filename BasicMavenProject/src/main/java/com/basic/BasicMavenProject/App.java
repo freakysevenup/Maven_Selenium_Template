36831f02e93cefce744cc204b4ede82041f94455
Jesse Derochie <jesse.a.derochie@gmail.com>
2017-12-08 13:34:09 -0500

    Initial commit of the Maven_Selenium_Template Project

---
+++ BasicMavenProject/src/main/java/com/basic/BasicMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.basic.BasicMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
